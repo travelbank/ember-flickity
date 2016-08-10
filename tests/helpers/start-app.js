9af1992b7fb2f55d77c549c872e92544a887d6e6
Dylan Foster <dylan947@gmail.com>
2016-08-10 09:31:10 -0700

    style: fix remaining lint errors

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,12 +1,13 @@
-import Ember from 'ember';
-import Application from '../../app';
-import config from '../../config/environment';
+import Ember from "ember";
+import Application from "../../app";
+import config from "../../config/environment";
 
 export default function startApp(attrs) {
   let application;
-
   let attributes = Ember.merge({}, config.APP);
-  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+
+  // use defaults, but you can override;
+  attributes = Ember.merge(attributes, attrs);
 
   Ember.run(() => {
     application = Application.create(attributes);
--- tests/integration/components/em-flickity-test.js
+++ tests/integration/components/em-flickity-test.js
@@ -3,7 +3,9 @@ import { expect } from "chai";
 import { describeComponent, it } from "ember-mocha";
 import hbs from "htmlbars-inline-precompile";
 
-describeComponent("em-flickity", "Integration: EmFlickityComponent", { integration: true }, function () {
+describeComponent("em-flickity", "Integration: EmFlickityComponent", {
+  integration: true
+}, function () {
   it("renders", function () {
     this.render(hbs`{{em-flickity}}`);
     expect(this.$()).to.have.length(1);

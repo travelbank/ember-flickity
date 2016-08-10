b7f4b6328251a90c496a760b0bb0289be0bde7e2
Dylan Foster <dylan947@gmail.com>
2016-08-10 09:50:28 -0700

    test(components): add some tests

--- tests/integration/components/em-flickity-test.js
+++ tests/integration/components/em-flickity-test.js
@@ -1,7 +1,12 @@
 /* jshint expr:true */
 import { expect } from "chai";
 import { describeComponent, it } from "ember-mocha";
+import { describe } from "mocha";
 import hbs from "htmlbars-inline-precompile";
+
+const NEXT_BUTTON = ".flickity-prev-next-button.next";
+const PREV_BUTTON = ".flickity-prev-next-button.previous";
+const PAGE_DOTS = ".flickity-page-dots";
 
 describeComponent("em-flickity", "Integration: EmFlickityComponent", {
   integration: true
@@ -9,5 +14,34 @@ describeComponent("em-flickity", "Integration: EmFlickityComponent", {
   it("renders", function () {
     this.render(hbs`{{em-flickity}}`);
     expect(this.$()).to.have.length(1);
+  });
+
+  describe("showSlides", function () {
+    it("hides flickity controls if false", function () {
+      this.render(hbs`{{em-flickity}}`);
+
+      expect(this.$(NEXT_BUTTON)).to.have.length(0);
+      expect(this.$(PREV_BUTTON)).to.have.length(0);
+    });
+
+    it("shows flickity controls if true", function () {
+      this.render(hbs`
+          {{#em-flickity showSlides=true}}
+            <div id="slide-1"></div>
+            <div id="slide-2"></div>
+          {{/em-flickity}}
+      `);
+
+      expect(this.$(NEXT_BUTTON)).to.have.length(1);
+    });
+  });
+
+  it("passes options to flickity", function () {
+    this.render(hbs`
+        {{#em-flickity pageDots=true showSlides=true}}
+        {{/em-flickity}}
+        `);
+
+    expect(this.$(PAGE_DOTS)).to.have.length(1);
   });
 });

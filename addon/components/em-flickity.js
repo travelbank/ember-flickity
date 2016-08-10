706e7862c94725b250ba3b2d72bf8e8af58fb2a0
Dylan Foster <dylan947@gmail.com>
2016-08-10 09:19:06 -0700

    chore(deps): add mocha and eslint

--- addon/components/em-flickity.js
+++ addon/components/em-flickity.js
@@ -1,72 +1,72 @@
-import Ember from 'ember';
-import layout from '../templates/components/em-flickity';
+import Ember from "ember";
+import layout from "../templates/components/em-flickity";
 
-const { $, computed, get, getProperties, set } = Ember;
+const { computed, get, getProperties, set } = Ember;
 
 export default Ember.Component.extend({
   layout,
-  classNames: ['flickity-wrapper'],
+  classNames: ["flickity-wrapper"],
   showSlides: false,
   widget: null,
 
   // some default flickity settings
-  cellAlign: 'center',
+  cellAlign: "center",
   contain: true,
   friction: 0.8,
   pageDots: false,
   selectedAttraction: 0.125,
   setGallerySize: false,
 
-  optionKeys: computed(function () {
+  optionKeys: computed(function getOptionKeys() {
     return [
-      'accessibility',
-      'adaptiveHeight',
-      'arrowShape',
-      'asNavFor',
-      'autoPlay',
-      'bgLazyLoad',
-      'draggable',
-      'gragThreshold',
-      'cellAlign',
-      'cellSelector',
-      'contain',
-      'freeScroll',
-      'freeScrollFriction',
-      'friction',
-      'groupCells',
-      'imagesLoaded',
-      'initialIndex',
-      'lazyLoad',
-      'pageDots',
-      'percentPosition',
-      'prevNextButtons',
-      'resize',
-      'rightToLeft',
-      'selectedAttraction',
-      'setGallerySize',
-      'watchCSS',
-      'wrapAround'
+      "accessibility",
+      "adaptiveHeight",
+      "arrowShape",
+      "asNavFor",
+      "autoPlay",
+      "bgLazyLoad",
+      "draggable",
+      "gragThreshold",
+      "cellAlign",
+      "cellSelector",
+      "contain",
+      "freeScroll",
+      "freeScrollFriction",
+      "friction",
+      "groupCells",
+      "imagesLoaded",
+      "initialIndex",
+      "lazyLoad",
+      "pageDots",
+      "percentPosition",
+      "prevNextButtons",
+      "resize",
+      "rightToLeft",
+      "selectedAttraction",
+      "setGallerySize",
+      "watchCSS",
+      "wrapAround"
     ];
   }),
 
   didInsertElement() {
-    if (get(this, 'showSlides')) {
-      set(this, 'widget', this.$().flickity(this._getOptions()));
+    if (get(this, "showSlides")) {
+      set(this, "widget", this.$().flickity(this._getOptions()));
     }
   },
 
   willDestroyElement() {
-    if (get(this, 'widget')) {
-      get(this, 'widget').flickity('destroy');
+    if (get(this, "widget")) {
+      get(this, "widget").flickity("destroy");
     }
   },
 
   _getOptions() {
-    const propKeys = get(this, 'optionKeys');
+    const propKeys = get(this, "optionKeys");
     const props = getProperties(this, ...propKeys);
 
     Object.keys(props).forEach(prop => {
-      if (!props[prop] && typeof props[prop] !== 'boolean') {
+      if (!props[prop] && typeof props[prop] !== "boolean") {
         delete props[prop];
       }
     });
--- tests/integration/components/em-flickity-test.js
+++ tests/integration/components/em-flickity-test.js
@@ -1,24 +1,11 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+/* jshint expr:true */
+import { expect } from "chai";
+import { describeComponent, it } from "ember-mocha";
+import hbs from "htmlbars-inline-precompile";
 
-moduleForComponent('em-flickity', 'Integration | Component | em flickity', {
-  integration: true
-});
-
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{em-flickity}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#em-flickity}}
-      template block text
-    {{/em-flickity}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+describeComponent("em-flickity", "Integration: EmFlickityComponent", { integration: true }, function () {
+  it("renders", function () {
+    this.render(hbs`{{em-flickity}}`);
+    expect(this.$()).to.have.length(1);
+  });
 });
